c8555382be9b2fd19ae9e2c1fb21112a1ec70413
viktor.chukholskiy <viktor.chukholskiy@cactussoft.biz>
2018-07-10 10:56:10 +0300

    Add check contract activity

--- app/src/main/java/biz/cactussoft/ethereumwallet/activities/MainActivity.kt
+++ app/src/main/java/biz/cactussoft/ethereumwallet/activities/MainActivity.kt
@@ -2,18 +2,17 @@ package biz.cactussoft.ethereumwallet.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import biz.cactussoft.ethereumwallet.R
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
 		btn_check_balance.setOnClickListener {
 			startActivity(CheckBalanceActivity.newIntent(this)); }
+		btn_check_contract.setOnClickListener {
+			startActivity(CheckContractActivity.newIntent(this)); }
 	}
 }
